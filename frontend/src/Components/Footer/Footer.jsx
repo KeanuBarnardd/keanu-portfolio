2c14c3aa73d9c8f33846f7cac88ddd8142138dbe
Keanu Barnard <keanu.barnardd@gmail.com>
2022-09-19 17:45:58 +1000

    Finish footer, about and navbar responsive

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -3,7 +3,69 @@ import React from "react";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div id="footer">Footer</div>;
+  return (
+    <div id="footer" className="app__flex">
+      <div className="app__container-width form__container">
+        <p className="tag">Contact</p>
+        <h1 className="head-text">
+          Get in <span>Touch</span>
+        </h1>
+
+        <div className="form__content">
+          <div className="form__right">
+            <p className="p-text">
+              Lorem ipsum dolor sit amet <span>consectetur</span> adipisicing elit. Inventore quod
+              nulla consequuntur ullam <span>laudantium</span> , dolorem rem culpa eum pariatur
+              porro.
+            </p>
+            <div className="form__grid">
+              <div className="form__contact">
+                <a href="">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+                <p className="p-text">KeanuBarnardd</p>
+              </div>
+              <div className="form__contact">
+                <a href="">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+                <p className="p-text">KeanuBarnard</p>
+              </div>
+              <div className="form__contact">
+                <i className="fa-regular fa-envelope"></i>
+                <p className="p-text">keanubarnardd@gmail.com</p>
+              </div>
+              <div className="form__contact">
+                <i className="fa-solid fa-phone"></i>
+                <p className="p-text">0455 847 718</p>
+              </div>
+            </div>
+            <p className="p-text farewell">
+              Looking forward to hearing from you, <span>Keanu Barnard</span>
+            </p>
+          </div>
+          <form action="">
+            <div className="form__input-container">
+              <p className="p-text">Name</p>
+              <input type="text" />
+            </div>
+            <div className="form__input-container">
+              <p className="p-text">Email</p>
+              <input type="text" />
+            </div>
+            <div className="form__input-container">
+              <p className="p-text">Message</p>
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
+            <button className="header-btn work">Send</button>
+          </form>
+        </div>
+        <div className="form__bottom">
+          <p className="p-text">@2022 Designed and built by Keanu_Barnard</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,57 @@
 import React from "react";
 import { NavHashLink, HashLink } from "react-router-hash-link";
+import { useState } from "react";
 import "./Navbar.scss";
-import { images } from "../../Constants";
 
 const Navbar = () => {
+  const [displayMobileNav, setDisplayMobileNav] = useState(false);
+
+  const displayNav = () => {
+    displayMobileNav === true ? setDisplayMobileNav(false) : setDisplayMobileNav(true);
+    console.log("Nav beeing clicked");
+    console.log(displayMobileNav);
+  };
+
   return (
-    <nav className="app__flex">
+    <nav className={`app__flex nav ${displayMobileNav === true ? "active" : ""}`}>
       <div className="app__container-width nav__content">
-        <HashLink className="logo" to={"/"}> <h1>K<span>B</span></h1> </HashLink>
-        <div className="nav__content-links">
-          <NavHashLink className={"nav-links"} to="/#about">
+        <HashLink
+          onClick={() => {
+            setDisplayMobileNav(false);
+          }}
+          className="logo"
+          to={"/"}
+        >
+          <h1>
+            K<span>B</span>
+          </h1>
+        </HashLink>
+        <div className={"nav__content-links"}>
+          <NavHashLink
+            onClick={() => {
+              setDisplayMobileNav(false);
+            }}
+            className={"nav-links"}
+            to="/#about"
+          >
             About
           </NavHashLink>
-          <NavHashLink className={"nav-links"} to="/#projects">
+          <NavHashLink
+            onClick={() => {
+              setDisplayMobileNav(false);
+            }}
+            className={"nav-links"}
+            to="/#projects"
+          >
             Projects
           </NavHashLink>
-          <NavHashLink className={"nav-links"} to="/#footer">
+          <NavHashLink
+            onClick={() => {
+              setDisplayMobileNav(false);
+            }}
+            className={"nav-links"}
+            to="/#footer"
+          >
             Contact
           </NavHashLink>
         </div>
@@ -23,11 +59,13 @@ const Navbar = () => {
           <a href="">
             <i className="fa-brands fa-github"></i>
           </a>
-          <HashLink to="/resume">
-            {" "}
+          <HashLink className="resume-link" to="/resume">
             <button className="resume-btn">Resume</button>{" "}
           </HashLink>
         </div>
+        <button onClick={displayNav} className="nav__button">
+          <i className="fa-solid fa-bars"></i>
+        </button>
       </div>
     </nav>
   );
